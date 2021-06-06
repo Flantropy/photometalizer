323101535e85e778cba229d2d79bf270b26f7833
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-06-06 04:44:00 +0300

    gallery fixed 2

--- config/settings.py
+++ config/settings.py
@@ -85,10 +85,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
