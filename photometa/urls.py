1d60a899603c801500dbcb54c98d4ebfc7ba420d
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-06-07 23:24:14 +0300

    Views refactored

--- photometa/urls.py
+++ photometa/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import home, PhotoDetailView, PhotoListView
+from .views import (
+    home,
+    ImageListView,
+    ImageDetailView,
+    ImageCreateView
+)
 
 
 urlpatterns = [
     path('', home, name='home'),
-    path('photos/', PhotoListView.as_view(), name='photos'),
-    path('photos/<int:pk>/', PhotoDetailView.as_view(), name='photo-detail')
+    path('photos/', ImageListView.as_view(), name='photos'),
+    path('photos/<int:pk>/', ImageDetailView.as_view(), name='photo-detail'),
+    path('photos/upload', ImageCreateView.as_view(), name='photo-upload'),
 ]
--- photometa/views.py
+++ photometa/views.py
@@ -1,45 +1,34 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.views.generic import DetailView, ListView
-from .forms import ImageUploadForm
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView
+)
+
 from .models import Image
+from .forms import ImageUploadForm
 
 
 def home(request):
     return render(request, 'photometa/base.html')
 
 
-def gallery(request):
-    if request.method == 'POST':
-        photo_upload_form = ImageUploadForm(
-            request.POST,
-            request.FILES,
-            instance=Image.objects.create(owner=request.user))
-        if photo_upload_form.is_valid():
-            photo_upload_form.save()
-            messages.success(request, f'Ваши фото успешно добавлены')
-            return redirect('gallery')
-    else:
-        photo_upload_form = ImageUploadForm()
-
-    user_photos = Image.objects.filter(owner=request.user)
-    user_photos = [obj.img for obj in user_photos if obj.img]
-    context = {
-        'user_photos': user_photos,
-        'photo_upload_form': photo_upload_form
-    }
-    return render(request, 'photometa/image_list.html', context)
-
-
-class PhotoListView(ListView):
+class ImageListView(ListView):
     model = Image
-    queryset = Image.objects.all()
-    extra_context = {
-        'images': Image.objects.all()
-    }
+
+    def get_queryset(self):
+        return Image.objects.filter(owner=self.request.user)
 
 
-class PhotoDetailView(DetailView):
+class ImageDetailView(DetailView):
     model = Image
-    template_name = 'photometa/photo_detail.html'
-    context_object_name = 'photo'
+
+
+class ImageCreateView(CreateView):
+    form_class = ImageUploadForm
+
+
+class ImageDeleteView(DeleteView):
+    pass
